f58ad7c210f0b02c8e1ac461b8d92a796bf5a9e1
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2021-05-14 15:03:34 +0200

    Írd meg a Sum35 függvényt, hogy lefusson a teszt!

--- Class1.cs
+++ Class1.cs
@@ -10,6 +10,8 @@ namespace Mymath
     public class Calc
     {
         public int Sum35()
+        //if we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. the sum of these multiples is 23.
+        //find the sum of all the multiples of 3 or 5 below 1000.
         {
             return 1;
         }
