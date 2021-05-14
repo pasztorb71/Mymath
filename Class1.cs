91b38ea1bfd00e7ae2c7385968269dad78caf11f
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2021-05-14 14:59:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymath
+{
+
+    public class Calc
+    {
+        public int Sum35()
+        {
+            return 1;
+        }
+    }
+}
